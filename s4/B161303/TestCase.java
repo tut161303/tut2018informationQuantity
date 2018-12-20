a6bb60214af42da6a27993b84548b0783fa90e88
tut161303 <nkgw98@icloud.com>
2018-12-20 17:21:02 +0900

    TestCase.java: for Frequencer

--- s4/B161303/TestCase.java
+++ s4/B161303/TestCase.java
@@ -123,7 +123,80 @@ public class TestCase {
 	catch(Exception e) {
 	    System.out.println("Exception occurred: STOP");
 	}
-
+	/*
+	  CASE 6
+	  チェック項目：TargetとSpaceに何もセットしない場合に0を返すか
+	*/
+	try {
+	    FrequencerInterface  myObject;
+	    int freq;
+	    System.out.println("CASE 6:checking s4.B161303.Frequencer");
+	    myObject = new s4.B161303.Frequencer();
+	    freq = myObject.frequency();
+	    System.out.print("\"H\" in \"Hi Ho Hi Ho\" appears "+freq+" times. ");
+	    if(0 == freq) { System.out.println("OK"); } else {System.out.println("WRONG"); }
+	}
+	catch(Exception e) {
+	    System.out.println("Exception occurred: STOP");
+	}
+	/*
+	  CASE 7
+	  チェック項目：TargetとSpaceの長さが0の場合に0を返すか
+	*/
+	try {
+	    FrequencerInterface  myObject;
+	    int freq;
+	    System.out.println("CASE 7:checking s4.B161303.Frequencer");
+	    myObject = new s4.B161303.Frequencer();
+	    myObject.setSpace("".getBytes());
+	    myObject.setTarget("".getBytes());
+	    freq = myObject.frequency();
+	    System.out.print("\"\" in \"\" appears "+freq+" times. ");
+	    if(4 == freq) { System.out.println("OK"); } else {System.out.println("WRONG"); }
+	}
+	catch(Exception e) {
+	    System.out.println("Exception occurred: STOP");
+	}
+	/*
+	  CASE 8
+	  チェック項目：配列内に日本語を使用してもfrequency()が正しく機能するか(環境によってエラーが出る場合がある)
+	*/
+		try {
+	    FrequencerInterface  myObject;
+	    int freq;
+	    System.out.println("CASE 8:checking s4.B161303.Frequencer");
+	    myObject = new s4.B161303.Frequencer();
+	    myObject.setSpace("はい ほ はい ほ".getBytes());
+	    myObject.setTarget("は".getBytes());
+	    freq = myObject.frequency();
+	    System.out.print("\"は\" in \"はい ほ はい ほ\" appears "+freq+" times. ");
+	    if(2 == freq) { System.out.println("OK"); } else {System.out.println("WRONG"); }
+	}
+	catch(Exception e) {
+	    System.out.println("Exception occurred: STOP");
+	}
+	/*
+	  Frequencer.javaの問題点:
+	  Spaceの最後の文字とTargetの最初の文字が一致しており、Targetの文字数が複数の場合を考える.
+	  この時,Targetの配列がSpaceの配列を超えてしまうためエラーが起きる.
+	  例:
+	  Space: aiueo
+	  Target     oi ←iが超えている
+	*/
+	try {
+	    FrequencerInterface  myObject;
+	    int freq;
+	    System.out.println("checking s4.B161303.Frequencer");
+	    myObject = new s4.B161303.Frequencer();
+	    myObject.setSpace("aiueo".getBytes());
+	    myObject.setTarget("oi".getBytes());
+	    freq = myObject.frequency();
+	    if(0 == freq) { System.out.println("OK"); } else {System.out.println("WRONG"); }
+	}
+	catch(Exception e) {
+	    System.out.println("Exception occurred: STOP");
+	}
+	
 	try {
 	    InformationEstimatorInterface myObject;
 	    double value;
